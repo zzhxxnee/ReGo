4c3533848583612c18e200dd02dd631eb170cd29
Gongdongjang <jeesoo0908@gmail.com>
2022-12-04 18:26:06 +0900

    #1 [Feat] add signin, signup, signout

---
+++ controllers/main.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  const title = "main";
+  if (req.session.user) {
+    const nickname = req.session.user["nickname"];
+    console.log(nickname);
+    res.render("main", {title: title, nickname: nickname});
+  } else {
+    res.render("loginMain", {title: title});
+  }
+});
+
+module.exports = router;
