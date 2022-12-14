6221b708078539f6c4c6fad484e623c7655a31a8
leejw-lu <jiwoojenny@naver.com>
2022-12-14 21:50:56 +0900

    #5 [Feat] Writing New Year's Goals

--- controllers/main.js
+++ controllers/main.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const pool = require("../db.js");
 
 router.get("/", (req, res) => {
   const title = "main";
@@ -10,5 +11,27 @@ router.get("/", (req, res) => {
     res.render("loginMain", {title: title});
   }
 });
+
+router.post("/write_goal", async (req, res, next) => {
+
+  const post=req.body;
+  console.log(post);
+
+  const goal_content =req.body.goal_content;
+  const nickname = req.session.user["nickname"];
+  
+  try {
+    const data = await pool.query(
+      `INSERT INTO goal(goal_user, goal_content) VALUES (?, ?)`,[nickname, goal_content]
+    );
+    console.log(data[0][0]);
+    res.render("main", {title: "main", nickname: nickname});
+   
+    } catch (err) {
+      console.error(err);
+    }
+
+});
+
 
 module.exports = router;
