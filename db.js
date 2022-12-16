c32d3b551dffdb3240c70ec8bd3f70fa26572cce
JIHYUNLEE <87045645+zzhxxnee@users.noreply.github.com>
2022-12-16 20:40:26 +0900

    Create db.js

---
+++ db.js
@@ -0,0 +1,12 @@
+const mysql = require("mysql2/promise");
+
+const pool = mysql.createPool({
+  host: "127.0.0.1",
+  port: "3306",
+  user: "root",
+  password: "1234",
+  database: "opensource",
+  dateStrings: "date",
+});
+
+module.exports = pool;
